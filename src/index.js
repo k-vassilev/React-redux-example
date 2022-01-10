369b3999854433e6d5e90d303453139d7b4a294f
Kristian Vassilev <kvasilev@devrix.com>
2022-01-10 17:15:45 +0200

    sample redux structure

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {createStore} from 'redux';
+import allReducers from './reducers';
 
+const store = createStore(allReducers);
 
 ReactDOM.render(<App />,document.getElementById('root'));
