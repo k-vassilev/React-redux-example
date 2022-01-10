51e22dfe8a7a9c501a6e60561c4bfbb805321087
Kristian Vassilev <kvasilev@devrix.com>
2022-01-10 17:02:05 +0200

    Simple redux example and readme

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import App from './App';
 import {createStore} from 'redux';
 
 
-//STORE -> GOBALIZED STATE
+//STORE -> GOBALIZED STATE - globalized state, holding the data used throughout the whole application
 
-//ACTION INCREMENT
+//ACTION INCREMENT - Describes what we want to do with regard to the state; A function which returns an object
 const increment = () => {
 	return {
 		type: 'INCREMENT',
@@ -21,7 +21,7 @@ const decrement = () => {
 	}
 }
 
-//REDUCER
+//REDUCER - Describes how the actions transform the state into the next state ; modifies store based on the chosen action
 const counter = (state = 0, action) => {
 	switch(action.type){
 		case "INCREMENT":
@@ -36,7 +36,7 @@ let store = createStore(counter);
 //Display in console
 store.subscribe(() => console.log(store.getState()));
 
-//DISPATCH - executes actions
+//DISPATCH - rigger. Action is dispatched to the reducer, reducer determines how to affect the state and then acts on it.
 store.dispatch(increment());
 
 ReactDOM.render(<App />,document.getElementById('root'));
