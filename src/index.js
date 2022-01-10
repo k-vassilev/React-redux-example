3179fe482a10e218b06a788a99a475e8d2031e74
Kristian Vassilev <kvasilev@devrix.com>
2022-01-10 17:10:05 +0200

    Added two reducers and changed structure

--- src/index.js
+++ src/index.js
@@ -3,40 +3,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import {createStore} from 'redux';
-
-
-//STORE -> GOBALIZED STATE - globalized state, holding the data used throughout the whole application
-
-//ACTION INCREMENT - Describes what we want to do with regard to the state; A function which returns an object
-const increment = () => {
-	return {
-		type: 'INCREMENT',
-	}
-}
-
-const decrement = () => {
-	return {
-		type: 'DECREMENT',
-	}
-}
-
-//REDUCER - Describes how the actions transform the state into the next state ; modifies store based on the chosen action
-const counter = (state = 0, action) => {
-	switch(action.type){
-		case "INCREMENT":
-			return state + 1;
-		case "DECREMENT":
-			return state - 1;
-	}
-}
-
-let store = createStore(counter);
-
-//Display in console
-store.subscribe(() => console.log(store.getState()));
-
-//DISPATCH - rigger. Action is dispatched to the reducer, reducer determines how to affect the state and then acts on it.
-store.dispatch(increment());
 
 ReactDOM.render(<App />,document.getElementById('root'));
